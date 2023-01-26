1e4e109d26809f15749c4d719df72d3bf07d16b5
Troll-Hub <122985117+Troll-Hub@users.noreply.github.com>
2023-01-26 17:09:36 -0300

    Update Key System.lua

--- Key System.lua
+++ Key System.lua
@@ -42,7 +42,7 @@ local Tab = Window:MakeTab({
 
 Tab:AddTextbox({
 	Name = "Enter Key System",
-	Default = "",
+	Default = "Password: troll-roblox",
 	TextDisappear = false,
 	Callback = function(Value)
 		_G.KeyInput = Value
