2e51752920e047722cc5eaabb7f77b81894c7c4a
Troll-Hub <122985117+Troll-Hub@users.noreply.github.com>
2023-01-25 21:05:44 -0300

    Update Key System.lua

--- Key System.lua
+++ Key System.lua
@@ -14,62 +14,6 @@ _G.KeyInput = "string"
 
 function MakeScriptHub()
         loadstring(game:HttpGet(('https://raw.githubusercontent.com/Troll-Hub/Scripts/main/Animal%20Simulator')))()
-        if game.PlaceId == 6104994594 then
-local Player = game.Players.LocalPlayer
-        --Load
-        local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-        OrionLib:MakeNotification({
-	Name = "Title!",
-	Content = "You are logging as "..Players.Name.."",
-	Image = "rbxassetid://4483345998",
-	Time = 10
-})
-        
-        --Main
-        local Window = OrionLib:MakeWindow({Name = "Troll Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Hubcfg", IntroEnabled = true, IntroText = "Troll Hub | Pelfering Pirates"})
-        
-        --Jogador
-        local JogadrTab = Window:MakeTab({
-	Name = "Jogador",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-        local Section = JogadorTab:AddSection({
-	Name = "Auto-Farm"
-})
-        JogadorTab:AddButton({
-	Name = "Hitbox",
-	Callback = function()
-      		loadstring(game:HttpGet(('https://raw.githubusercontent.com/Troll-Hub/Scripts/main/hitbox.lua')))()
-  	end    
-})
-        
-        --Others
-        local OthersTab = Window:MakeTab({
-	Name = "Others",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-        local Section = OthersTab:AddSection({
-	Name = "Others Scripts"
-})
-        OthersTab:AddButton({
-	Name = "RTX",
-	Callback = function()
-      		loadstring(game:HttpGet(('https://raw.githubusercontent.com/Troll-Hub/Scripts/main/RTX.lua')))()
-  	end    
-})
-        OthersTab:AddButton({
-	Name = "IY",
-	Callback = function()
-      		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-  	end    
-})
-
-
-end
-OrionLib:Init()
-        end
 
 function CorrectKeyNotification()
         OrionLib:MakeNotification({
