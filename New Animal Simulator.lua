abfb9f26d55dd87a6504bbc7693017b0b2188456
Troll-Hub <122985117+Troll-Hub@users.noreply.github.com>
2023-09-15 19:03:20 -0300

    NEW UPDATE?!!
    Sorry for the delay, 🤑🤑🤑

--- New Animal Simulator.lua
+++ New Animal Simulator.lua
@@ -11,7 +11,6 @@ if game.PlaceId == 5712833750 then
 _G.Coins = true
 _G.LavaMonster = true
 _G.Griffin = true
-_G.Kdummies = true
         
         ---function
 function Coins()
@@ -20,19 +19,6 @@ function Coins()
         wait(0.00001)
         end
 end
-
-function Dummies()
-while _G.Kdummies == true do
-local args = {
-    [1] = workspace.MAP:FindFirstChild("5k_dummies").Dummy2.Humanoid,
-    [2] = 5
-}
-
-game:GetService("ReplicatedStorage").jdskhfsIIIllliiIIIdchgdIiIIIlIlIli:FireServer(unpack(args))
-wait(0.001)
-end
-end
-
 function LavaMonster()
         while _G.LavaMonster == true do
         local args = {
@@ -59,7 +45,7 @@ end
         --Jogador
         local JogadorTab = Window:MakeTab({
 	Name = "Jogador",
-	Icon = "rbxassetid://12473074641",
+	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
         local Section = JogadorTab:AddSection({
@@ -73,15 +59,6 @@ end
 Coins()
 	end    
 })
-	JogadorTab:AddToggle({
-	Name = "5K dummies farm",
-	Default = false,
-	Callback = function(Value)
-		_G.Kdummies = Value
-				Dummies()
-	end    
-})
-	
 
         --Others
         local OthersTab = Window:MakeTab({
@@ -152,6 +129,89 @@ rootPart.CFrame = CFrame.new(148.361176, 669.264893, 1441.97571, -0.023542881, 0
 Griffin()
 	end    
 })
+
+BetaTab:AddButton({
+  Name = "Enable Fly"
+  Defaut = false
+  Callback = function()
+          -- Obtén el jugador local
+
+local jugadorLocal = game.Players.LocalPlayer
+
+local character = jugadorLocal.Character
+
+-- Verifica que el jugador tenga un personaje y que no esté volando actualmente
+if character and not character:FindFirstChild("HumanoidFlying") then
+    -- Crea un objeto HumanoidFlying en el personaje del jugador
+    local flying = Instance.new("HumanoidFlying")
+    flying.MaxSlope = 89  -- Ajusta el ángulo máximo de inclinación
+    flying.Parent = character.Humanoid
+    jugadorLocal.PlayerGui:SetTopbarTransparency(0)  -- Muestra la barra superior de nuevo
+
+    -- Activa el vuelo
+    character.HumanoidRootPart.Anchored = false
+    character.HumanoidRootPart.Velocity = Vector3.new(0, 0, 0)
+    character.HumanoidRootPart.CFrame = CFrame.new(character.HumanoidRootPart.Position)
+
+    -- Desactiva la gravedad
+    character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.Physics, false)
+
+    -- Configura el control de vuelo en el dispositivo móvil
+    if jugadorLocal:GetMouse() then
+        local touchControl = Instance.new("TouchControl", jugadorLocal.PlayerGui)
+        touchControl.Name = "FlightControl"
+        touchControl.Size = UDim2.new(0, 200, 0, 200)  -- Ajusta el tamaño según tus preferencias
+        touchControl.Position = UDim2.new(0, 0, 1, -200)  -- Coloca el control en la esquina inferior izquierda
+        touchControl.Style = Enum.TouchControlStyle.Thumbstick
+        touchControl.Visible = true
+
+        -- Conecta la entrada del usuario al control de vuelo
+        touchControl.TouchPan:Connect(function(direction)
+            flying:SetDesiredMoveDirection(Vector3.new(direction.x, 0, direction.y))
+        end)
+    end
+end
+
+  end
+})
+BetaTab:AddButton({
+  Name = "Disable Fly"
+  Defaut = false
+  Callback = function()
+    -- Obtén el jugador local
+
+local jugadorLocal = game.Players.LocalPlayer
+
+local character = jugadorLocal.Character
+
+-- Función para desactivar el vuelo
+local function desactivarVuelo()
+    -- Verifica si el jugador tiene un objeto HumanoidFlying
+    if character and character:FindFirstChild("HumanoidFlying") then
+        -- Elimina el objeto HumanoidFlying del personaje
+        character.HumanoidFlying:Destroy()
+        
+        -- Activa la gravedad nuevamente
+        character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.Physics, true)
+        
+        -- Elimina el control de vuelo en el dispositivo móvil (si existe)
+        local flightControl = jugadorLocal.PlayerGui:FindFirstChild("FlightControl")
+        if flightControl then
+            flightControl:Destroy()
+        end
+    end
+end
+
+-- Conecta la función a un evento que indica cuando el jugador quiere desactivar el vuelo
+-- Por ejemplo, puedes hacer que esto ocurra cuando el jugador toca un botón "Detener vuelo" en la interfaz de usuario
+local botonDetenerVuelo = script.Parent:FindFirstChild("BotonDetenerVuelo") -- Ajusta el nombre según tu botón
+if botonDetenerVuelo then
+    botonDetenerVuelo.MouseButton1Click:Connect(desactivarVuelo)
+end
+
+})
+end
+
         
         
 end
